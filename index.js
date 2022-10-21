e42a4dfed77fc099576dead7057f6d345b062fed
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-10-21 16:49:05 +0900

    'Fix'

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ const {KalmanFilter} = require('kalman-filter');
 const kf = new KalmanFilter();
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-let Dir = `./kmou_003`
-let Filename = `/kmou_dataset003`
-let DownName = `/kalman_dataset003`
-let SaveDir = `./kmou_003_filter`
+let Dir = `./kmou_009`
+let Filename = `/kmou_dataset009`
+let DownName = `/kalman_dataset009`
+let SaveDir = `./kmou_009_filter`
 
-for(let i=0; i<=20; i++){
-    A(i)
+for(let i=1; i<=1; i++){
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            A(i);
+        }, 1000);          
+    })
+    .then((data) => {
+    	console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    });
 }
 
 // async function makeCSV (results) {
@@ -25,11 +32,11 @@ for(let i=0; i<=20; i++){
 //     console.log("makeCSV 함수")
 // };
 async function A (num){
-let FILE_NAME = `${Dir}${Filename}_${num}.csv`
+let FILE_NAME = `${Dir}${Filename}-${num}.csv`
+//let FILE_NAME = `${Dir}${Filename}_${num}.csv`
 let csvPath = path.join(__dirname,FILE_NAME);
 let csv = fs.readFileSync(csvPath,"utf-8")
 let rows = csv.split("\r\n")
-console.log(rows.slice(0,5))
 // 저장된 파일이 빈 csv파일일때, 아래것으로 실행
 
 //let rows = csv.split("\n")
@@ -50,9 +57,9 @@ for (const i in rows){
     } else {
         let row_data = {}
         for (const index in columnTitle) {
-            changeString = columnTitle[index].replace(/ /g, "_").trim()
+            changeString = columnTitle[index].replace(/ /g, "_").replace(/-/g,"_").trim()
             const title = changeString.trim()
-            if(index === "1" || index === "2"){
+            if(index === "1" || index === "2" || index === "3" ){
             row_data[title] = data[index]
             } else {
             row_data[title] = Number(data[index])
@@ -60,60 +67,127 @@ for (const i in rows){
             //row_data[title] = 1
         }
         results.push(row_data)
-        console.log(results)
     }
 }
-let A_data = []; 
+console.log(results)
+
+let A_data = [];
 for(let e in results ){
-    A_data.push(results[e].Tachometer)
+    A_data.push(results[e].x)
 }
 let B_data = []; 
 for(let e in results ){
-    B_data.push(results[e].x)
+    B_data.push(results[e].y)
 }
 let C_data = []; 
 for(let e in results ){
-    C_data.push(results[e].Humidity)
+    C_data.push(results[e].z)
 }
-Overall_data = kf.filterAll(Overall_data)
-Temperature_data = kf.filterAll(Temperature_data)
-Humidity_data = kf.filterAll(Humidity_data)
+
+// let A_data = [];
+// for(let e in results ){
+//     A_data.push(results[e].Tachometer)
+// }
+// let B_data = []; 
+// for(let e in results ){
+//     B_data.push(results[e].Engine_body_vibration_at_fore_top_longitudinal_direction)
+// }
+// let C_data = []; 
+// for(let e in results ){
+//     C_data.push(results[e].Engine_body_vibration_at_fore_top_transverse_direction)
+// }
+// let D_data = []; 
+// for(let e in results ){
+//     D_data.push(results[e].Engine_body_vibration_at_fore_top_vertical_direction)
+// }
+// let E_data = []; 
+// for(let e in results ){
+//     E_data.push(results[e].Engine_body_vibration_at_aft_top_transverse_direction)
+// }
+
+A_data = kf.filterAll(A_data)
+B_data = kf.filterAll(B_data)
+C_data = kf.filterAll(C_data)
+
+// A_data = kf.filterAll(A_data)
+// B_data = kf.filterAll(B_data)
+// C_data = kf.filterAll(C_data)
+// D_data = kf.filterAll(D_data)
+// E_data = kf.filterAll(E_data)
 
 // let recursion = 0
 // results.map((e) =>{
     
 
+// for (let i in results){
+//     results[i].Tachometer = A_data[i][0]
+//     results[i].Engine_body_vibration_at_fore_top_longitudinal_direction = B_data[i][0]
+//     results[i].Engine_body_vibration_at_fore_top_transverse_direction = C_data[i][0]
+//     results[i].Engine_body_vibration_at_fore_top_vertical_direction = D_data[i][0]
+//     results[i].Engine_body_vibration_at_aft_top_transverse_direction = E_data[i][0]
+// }
 for (let i in results){
-    results[i].Overall = Overall_data[i][0]
-    results[i].Temperature = Temperature_data[i][0]
-    results[i].Humidity = Humidity_data[i][0]
+    results[i].x = A_data[i][0]
+    results[i].y = B_data[i][0]
+    results[i].z = C_data[i][0]
 }
-// await makeCSV(results)
+// console.log(results.slice(0,5))
+
+// // await makeCSV(results)
 console.log(`Kalman ${FILE_NAME} ... done`)
-// CSV(results,num)
-
+await CSV(results,num)
 }
 
 
-function CSV (data,i){
-    const csvWriter = createCsvWriter({
-        path: `${SaveDir}${DownName}-${i}.csv`,
-        header : [
-            {id:"MMSI", title: "MMSI"},
-            {id:"ShipName", title: "ShipName"},
-            {id:"DataInfo", title: "DataInfo"},
-            {id:"x", title: "x"},
-            {id:"y", title: "y"},
-            {id:"z", title: "z"},
-            {id:"Overall", title: "Overall"},
-            {id:"Temperature", title: "Temperature"},
-            {id:"Humidity", title: "Humidity"}
-        ]
+async function CSV (data,i){
+    // console.log(Object.keys(data[0]))
+    let title = Object.keys(data[0])
+    // const csvWriter = createCsvWriter({
+    //     path: `${SaveDir}${DownName}-${i}.csv`,
+    //     header : [
+    //         {id:title[0], title: "MMSI"},
+    //         {id:title[1], title: "ShipName"},
+    //         {id:title[2], title: "DataInfo"},
+    //         {id:title[3], title: "Tachometer"},
+    //         {id:title[4], title: "Engine_body_vibration_at_fore_top_longitudinal_direction"},
+    //         {id:title[5], title: "Engine_body_vibration_at_fore_top_transverse_direction"},
+    //         {id:title[6], title: "Engine_body_vibration_at_fore_top_vertical_direction"},
+    //         {id:title[7], title: "Engine_body_vibration_at_aft_top_transverse_direction"},
+    //         // {id:title[8], title: "Temperature"},
+    //         // {id:title[9], title: "Humidity"}
+    //     ]
+    // })
+    // await csvWriter.writeRecords(data)
+    // .then(()=>{
+    //     console.log("Save ... Done ")
+    // })
+    let writeStream = fs.createWriteStream(`${SaveDir}${DownName}-${i}.csv`)
+    console.log(title)
+data.forEach((data, index) => {
+    let newLine = []
+    if(index === 0){
+        newLine.push(title)
+        writeStream.write(newLine.join(',')+ '\r\n', () => {
+            // a line was written to stream
+        })
+    }else{
+    for(let el in title){
+    console.log(data.title[el])
+    newLine.push(data[index][el])
+    }
+    writeStream.write(newLine.join(',')+ '\r\n', () => {
+        // a line was written to stream
     })
-    csvWriter.writeRecords(data)
-    .then(()=>{
-        console.log("Save ... Done ")
-    })
+}
+})
+
+writeStream.end()
+
+writeStream.on('finish', () => {
+    console.log('finish write stream, moving along')
+}).on('error', (err) => {
+    console.log(err)
+})
 }
 
 // //
